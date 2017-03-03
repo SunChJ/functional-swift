b1625b3eba6830646ca9a112b75490eff19d186c
SunCJ <350752900@qq.com>
2017-03-03 14:05:28 +0800

    update: Example

--- Fundational-Swift/Map,Filter,Reduce/MapFilterReduce.playground/Contents.swift
+++ Fundational-Swift/Map,Filter,Reduce/MapFilterReduce.playground/Contents.swift
@@ -43,7 +43,46 @@ func increamentArray2(xs: [Int]) -> [Int] {
 increamentArray2(xs: Array(repeating: 1, count: 5))
 doubleArray2(xs: Array(repeating: 1, count: 5))
 
-// 然而代码任然不像想象中那么灵活。 假设我们像得到一个Bool的新数组，表示原数组中对应的数字是否为偶数
-//func isEvenArray(xs: [Int]) -> [Bool] {
-//    return computeIntArray(xs: xs) { x in x%2 == 0}
-//}
+// 由于扩展性不佳， 我们使用范型。
+// computeBoolArray和computeIntArray的定义是相同的，唯一的区别在于类型签名（type signature）。
+func genericComputeArray1<T>(xs: [Int], transform: (Int) -> T) -> [T] {
+    var result: [T] = []
+    for x in xs {
+        result.append(transform(x))
+    }
+    return result
+}
+
+func genericComputeArray<T>(xs: [Int], transform: (Int) -> T) -> [T] {
+    return xs.map(transform)
+}
+
+let result = genericComputeArray(xs: Array(repeating: 1, count: 5), transform: {x in x+1})
+print(result)
+
+
+struct City {
+    let name: String
+    let population: Int
+}
+let paris = City(name: "Paris", population: 2241)
+let madrid = City(name: "Madrid", population: 3165)
+let amsterdam = City(name: "Amsterdam", population: 827)
+let berlin = City(name: "Berlin", population: 3562)
+
+let cities = [paris, madrid, amsterdam, berlin]
+
+extension City {
+    func cityByScalingPopulation() -> City {
+        return City(name: name, population: population * 1000)
+    }
+}
+
+[1,2,3].reduce(1, *)
+let display = cities.filter{$0.population > 1000}
+    .map{$0.cityByScalingPopulation()}
+    .reduce("Citi: Population"){ result, c in
+        return result + "\n" + "\(c.name): \(c.population)"
+}
+
+print(display)
